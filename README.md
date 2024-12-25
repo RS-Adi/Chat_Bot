4b9ca35ea0c5c2715d0ebb98e2a655ae0f53443e
Sri Adi Narayana. Repudi <123529692+RS-Adi@users.noreply.github.com>
2024-12-24 23:57:31 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chatbot Frontend
+A simple chatbot frontend for plugging in my bots.
+
+# How to run
+1. Prerequisites: 
+    * Visual Studio Code 
+    * [Live Server extension](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+2. Clone this project
+3. Open this folder in vscode and run bot.html using Live Server extension.
+
+
+
+## Screenshot
+![enter image description here](static/img/chat_sample.png)
+
+[Credits to the original author](https://github.com/PandaWhoCodes/chatbot-frontend)
+
---
+++ main.js
@@ -0,0 +1,136 @@
+/**
+ * Returns the current datetime for the message creation.
+ */
+function getCurrentTimestamp() {
+	return new Date();
+}
+
+/**
+ * Renders a message on the chat screen based on the given arguments.
+ * This is called from the `showUserMessage` and `showBotMessage`.
+ */
+function renderMessageToScreen(args) {
+	// local variables
+	let displayDate = (args.time || getCurrentTimestamp()).toLocaleString('en-IN', {
+		month: 'short',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	});
+	let messagesContainer = $('.messages');
+
+	// init element
+	let message = $(`
+	<li class="message ${args.message_side}">
+		<div class="avatar"></div>
+		<div class="text_wrapper" style="max-width:600px">
+			<div class="text">
+				${args.text}
+			</pre>
+			</div>
+			<div class="timestamp">${displayDate}</div>
+		</div>
+	</li>
+	`);
+
+	// add to parent
+	messagesContainer.append(message);
+
+	// animations
+	setTimeout(function () {
+		message.addClass('appeared');
+	}, 0);
+	messagesContainer.animate({ scrollTop: messagesContainer.prop('scrollHeight') }, 300);
+}
+
+/* Sends a message when the 'Enter' key is pressed.
+ */
+$(document).ready(function() {
+    $('#msg_input').keydown(function(e) {
+        // Check for 'Enter' key
+        if (e.key === 'Enter') {
+            // Prevent default behaviour of enter key
+            e.preventDefault();
+			// Trigger send button click event
+            $('#send_button').click();
+        }
+    });
+});
+
+/**
+ * Displays the user message on the chat screen. This is the right side message.
+ */
+function showUserMessage(message, datetime) {
+	renderMessageToScreen({
+		text: message,
+		time: datetime,
+		message_side: 'right',
+	});
+}
+
+/**
+ * Displays the chatbot message on the chat screen. This is the left side message.
+ */
+function showBotMessage(message, datetime) {
+	renderMessageToScreen({
+		text: message,
+		time: datetime,
+		message_side: 'left',
+	});
+}
+
+/**
+ * Get input from user and show it on screen on button click.
+ */
+$('#send_button').on('click', function (e) {
+	// get and show message and reset input
+	const userQuestion = $('#msg_input').val()
+	console.log(userQuestion)
+	showUserMessage($('#msg_input').val());
+	$('#msg_input').val('');
+
+	//perform API Call
+	var settings = {
+		"url": "http://127.0.0.1:5000/chatservice",
+		"method": "POST",
+		"timeout": 0,
+		"headers": {
+		  "Content-Type": "application/json"
+		},
+		"data": JSON.stringify({
+		  "question": userQuestion
+		}),
+	  };
+	  
+	  const res = $.ajax(settings).done(function (response) {
+		console.log(response);
+		showBotMessage("<pre style='font-family: system-ui;white-space: pre-line;word-break: break-word;'>" + response.response.answer + "</pre>");
+		return response
+	  });
+	
+});
+
+/**
+ * Returns a random string. Just to specify bot message to the user.
+ */
+function randomstring(length = 20) {
+	let output = '';
+
+	// magic function
+	var randomchar = function () {
+		var n = Math.floor(Math.random() * 62);
+		if (n < 10) return n;
+		if (n < 36) return String.fromCharCode(n + 55);
+		return String.fromCharCode(n + 61);
+	};
+
+	while (output.length < length) output += randomchar();
+	return output;
+}
+
+/**
+ * Set initial bot message to the screen for the user.
+ */
+$(window).on('load', function () {
+	showBotMessage('Welcome to Rockwell Automation Machinery. Type your query here');
+});
